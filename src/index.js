2e0d305394fe8ba93ecb0987f83dc55d39365901
Charles <vx.charles@gmail.com>
2021-03-31 16:04:09 -0300

    Alterando a configuração da store e os imports

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import multi from 'redux-multi'
-import reducers from './store/reducers/reducers'
+import configureStore from './store/configureStore'
+// import { createStore, applyMiddleware } from 'redux'
+// import thunk from 'redux-thunk' // usado para funções async nas actions quando for fazer o dispatch
+// import reducers from './store/reducers/reducers'
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = applyMiddleware(thunk, multi)(createStore)(reducers, devTools)
+// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+// const store = applyMiddleware(thunk)(createStore)(reducers, devTools)
+
+ const store = configureStore()
 
 ReactDOM.render(
     <React.StrictMode>
@@ -21,8 +23,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

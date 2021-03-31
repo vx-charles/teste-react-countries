b2da084e9ff3ecfb6118d42b3eb25c619e70078b
Charles <vx.charles@gmail.com>
2021-03-31 16:14:51 -0300

    Alterando texto da Home

--- src/template/Homepage.jsx
+++ src/template/Homepage.jsx
@@ -5,8 +5,11 @@ const Homepage = props => {
     return (
         <div className="homepage">
             <p>Olá, neste site mostraremos algumas informações sobre os <b>países</b> do mundo.</p>
-            <p>Acesso o menu ao lado para buscar informações dos países.</p>
-            <p><b>OBS:</b> Desculpe-nos, o site ainda está em construção.</p>
+            <p>Acesse o <b>menu</b> ao lado para buscar informações básicas dos países.</p>
+            <p>
+                <b>Repositório no GitHub para mais informações do código: </b>
+                <a href="https://github.com/vx-charles/teste-react-countries/" target="_blank">https://github.com/vx-charles/teste-react-countries/</a>
+            </p>
         </div>
     )
 }

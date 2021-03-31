8a39b36d065d0e04da4015b1bcfe4e1d48160803
Charles <vx.charles@gmail.com>
2021-03-31 16:11:38 -0300

    Adcionado uma action de search()

--- src/store/actions/countryActions.js
+++ src/store/actions/countryActions.js
@@ -5,6 +5,47 @@ const URL = 'https://countries-274616.ew.r.appspot.com/'
 //     payload: event.target.value
 // })
 
+export const search = () => { // para pesquisar tudo quando inicializar
+
+    const text = `( name: "${sessionStorage.getItem('nome_pais')}" )`
+
+    return dispatch => {
+        fetch(URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                query: `
+                    query {
+                        Country${text} {
+                            _id
+                            name
+                            capital
+                            area
+                            convertedArea {
+                                unit
+                            }
+                            flag {
+                                country {
+                                    population
+                                    flag {
+                                        svgFile
+                                    }
+                                    topLevelDomains {
+                                        name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                `
+            }),
+        })
+        .then(res => res.json())
+        .then(res => dispatch({ type: 'ONE_SEARCHED', payload: res.data.Country }))
+    }
+}
+
+
 export const searchAll = () => { // para pesquisar tudo quando inicializar
 
     return dispatch => {
